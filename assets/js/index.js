c49bfb4e28f0f74b6b8c5dbcdfe89a061b54309a
William Trindade <williamtrindade777@gmail.com>
2019-03-11 15:04:23 -0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,22 @@
-// comando de decisao
-var nome = 'theo';
+const config = {
+	'site_name' : 'William Trindade',
+	'initial_year' : 2019
 
-// entrada de dados
-var idade = prompt("Qual sua idade");
+}
+
+function makeCopyright() {
+	const d = new Date()
+	if(config['initial_year'] != d.getFullYear()) {
+		var year = " " + config['initial_year'] +" - "+ d.getFullYear()
+	} else {
+		var year = " " + config['initial_year']
+	}
+	document.querySelector('#copy').innerHTML = 
+	"© " + config['site_name'] + year + " Todos os direitos reservados."
+}
+
+function index() {
+	makeCopyright()
+}
+
+index()
