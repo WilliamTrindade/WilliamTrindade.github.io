32219c98de820a2211d123e6252c2d1dae95d884
williamtrindade <williamtrindade777@gmail.com>
2020-05-31 00:55:39 -0300

    Nova versão do site 1.0

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,15 @@
 const config = {
 	'site_name' : 'William Trindade',
 	'initial_year' : 2019
-
 }
 
 function makeCopyright() {
 	const d = new Date()
-	if(config['initial_year'] != d.getFullYear()) {
+	if (config['initial_year'] != d.getFullYear()) {
 		var year = " " + config['initial_year'] +" - "+ d.getFullYear()
 	} else {
 		var year = " " + config['initial_year']
 	}
-	document.querySelector('#copy').innerHTML = 
-	"© " + config['site_name'] + year + " Todos os direitos reservados."
+	document.querySelector('#copy').innerHTML = "© " + config['site_name'] + year + " Todos os direitos reservados."
 }
-
-function index() {
-	makeCopyright()
-}
-
-index()
+makeCopyright()
